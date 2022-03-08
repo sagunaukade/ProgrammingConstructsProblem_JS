8089864172b49e03988d425d9455cfa7c31ae672
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-03-08 11:46:04 +0530

    [Saguna]Added Sequence Practice Problem

---
+++ ProgrammingConstructsProblem_JS/SequencePracticeProblem/TwoRandomDiceNum.js
@@ -0,0 +1,5 @@
+// 3 Add two Random Dice Number and Print the Result
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+console.log("dice1: " + dice1, "dice2: " + dice2);
+console.log("sum: " + (dice1 + dice2));
