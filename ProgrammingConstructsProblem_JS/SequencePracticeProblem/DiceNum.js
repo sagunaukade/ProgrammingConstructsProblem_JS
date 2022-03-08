898815da7824f0a6a062819232bc9e3c8fdc0aae
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-03-08 11:48:28 +0530

    Delete ProgrammingConstructsProblem_JS directory

--- ProgrammingConstructsProblem_JS/SequencePracticeProblem/DiceNum.js
+++
@@ -1,3 +0,0 @@
-// 2 Use REPL Use Random to get Dice Number between 1 to 6
-let dice = Math.floor(Math.random() * 6) + 1;
-console.log("Random number " + dice);
--- ProgrammingConstructsProblem_JS/SequencePracticeProblem/SingleDigit.js
+++
@@ -1,3 +0,0 @@
-//Random Function Math.floor(Math.random() * 10)to get Single Digit
-let random = Math.floor(Math.random() * 10);
-console.log("Random number " +random);
